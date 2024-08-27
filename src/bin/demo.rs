0bebdd4eb47fd5e518dc6979cfc46119debd166a
deparr <dparrott@protonmail.com>
2024-08-27 17:38:07 -0600

    feat: sdl key events

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem.window("rust-sdl2 demo", 400, 200)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
@@ -20,7 +20,7 @@ fn main() -> Result<(), String> {
     canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
-    let mut r = sdl2::rect::Rect::new(100, 100, 30, 30);
+    let mut r = sdl2::rect::Rect::new(0, 100, 30, 30);
     'running: loop {
         i = (i + 1) % 255;
         canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
@@ -37,7 +37,7 @@ fn main() -> Result<(), String> {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         r.set_y(100);
         let _ = canvas.fill_rect(r);
-        r.set_y(300);
+        r.set_y(0);
         let _ = canvas.fill_rect(r);
         // The rest of the game loop goes here...
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use chip8::Chip8;
 
 
@@ -6,38 +8,106 @@ fn main() {
     // init gfx and key read contexts
     let sdl_context = sdl2::init().unwrap();
     let video_context = sdl_context.video().unwrap();
-    let window = video_context.window("chip8", 200, 100)
+    let window = video_context.window("chip8", 400, 200)
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
-
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
     let mut comp = Chip8::new().with_mode(chip8::StepMode::Debug);
     let prog = std::fs::read("./roms/pong2.c8").unwrap();
     comp.load(&prog).unwrap();
     let mut cc = 0;
-    loop {
+    'render: loop {
         match comp.step() {
             Ok(_) => {}
             Err(e) => {
-                panic!("emu step fail: {} on cc {}", e, cc+1);
+                panic!("emu step fail: {} on cc {}", e, cc + 1);
             }
         }
         cc += 1;
 
         if comp.draw {
-            if comp.step_mode == chip8::StepMode::Debug {
-                debug_render(&comp.gfx);
-            }
+            // if comp.step_mode == chip8::StepMode::Debug {
+            //     debug_render(&comp.gfx);
+            // }
             comp.draw = false;
         }
 
-        // set keys
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'render;
+                }
+                Event::KeyDown { keycode: Some(key), .. } => {
+                    let key = match key {
+                        Keycode::Num1 => 0x1,
+                        Keycode::Num2 => 0x2,
+                        Keycode::Num3 => 0x3,
+                        Keycode::Num4 => 0xc,
+
+                        Keycode::Q => 0x4,
+                        Keycode::W => 0x5,
+                        Keycode::E => 0x6,
+                        Keycode::R => 0xd,
+
+                        Keycode::A => 0x7,
+                        Keycode::S => 0x8,
+                        Keycode::D => 0x9,
+                        Keycode::F => 0xe,
+
+                        Keycode::Z => 0xa,
+                        Keycode::X => 0x0,
+                        Keycode::C => 0xb,
+                        Keycode::V => 0xf,
+                        _ => 16,
+                    };
+
+
+                    if key <= 15 {
+                        comp.key_down(key);
+                    }
+                },
+                Event::KeyUp { keycode: Some(key), .. } => {
+                    let key = match key {
+                        Keycode::Num1 => 0x1,
+                        Keycode::Num2 => 0x2,
+                        Keycode::Num3 => 0x3,
+                        Keycode::Num4 => 0xc,
+
+                        Keycode::Q => 0x4,
+                        Keycode::W => 0x5,
+                        Keycode::E => 0x6,
+                        Keycode::R => 0xd,
+
+                        Keycode::A => 0x7,
+                        Keycode::S => 0x8,
+                        Keycode::D => 0x9,
+                        Keycode::F => 0xe,
+
+                        Keycode::Z => 0xa,
+                        Keycode::X => 0x0,
+                        Keycode::C => 0xb,
+                        Keycode::V => 0xf,
+                        _ => 16,
+                    };
+
+
+                    if key <= 15 {
+                        comp.key_up(key);
+                    }
+                },
+                _  => {},
+
+            }
+        }
+        canvas.present();
     }
 }
 
