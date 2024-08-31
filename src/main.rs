4d483c2fab3e7ec4242e8ea2ecbb546da54149e4
deparr <dparrott@protonmail.com>
2024-08-31 00:05:43 -0600

    feat: colors

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use std::time::Duration;
+use std::u32;
 
 use chip8::{Chip8, StepMode};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::rect::Rect;
+use sdl2::render::{Texture, TextureCreator};
+use sdl2::video::{Window, WindowContext};
 
 struct Opts {
     mode: StepMode,
     file: String,
     tickrate: u32,
+    fg: u32,
+    bg: u32,
 }
 
+// TODO this sucks
 fn parse_cl() -> Result<Opts, String> {
     let args: Vec<String> = std::env::args().collect();
     let mode = match args.iter().position(|e| e == "-m" || e == "--mode") {
@@ -41,11 +47,29 @@ fn parse_cl() -> Result<Opts, String> {
         None => 20,
     };
 
+    let fg = match args.iter().position(|e| e == "--fg") {
+        Some(idx) => match args.get(idx + 1) {
+            Some(val) => u32::from_str_radix(val, 16).unwrap(),
+            None => return Err("Found --fg option, but no color val".into()),
+        },
+        None => 0xffffff,
+    };
+
+    let bg = match args.iter().position(|e| e == "--bg") {
+        Some(idx) => match args.get(idx + 1) {
+            Some(val) => u32::from_str_radix(val, 16).unwrap(),
+            None => return Err("Found --bg option, but no color value".into()),
+        },
+        None => 0x0,
+    };
+
     let file = file.to_owned();
     Ok(Opts {
         mode,
         file,
         tickrate,
+        fg,
+        bg,
     })
 }
 
@@ -69,36 +93,12 @@ fn main() -> Result<(), String> {
     let creator = canvas.texture_creator();
     let tex_w = win_width / 64;
     let tex_h = win_height / 32;
-    let mut black = creator
-        .create_texture_streaming(PixelFormatEnum::RGB24, tex_w, tex_h)
-        .map_err(|e| e.to_string())?;
-    black.with_lock(None, |buf: &mut [u8], pitch: usize| {
-        for y in 0..tex_h {
-            for x in 0..tex_w {
-                let offset = y as usize * pitch + x as usize * 3;
-                buf[offset] = 0;
-                buf[offset + 1] = 0;
-                buf[offset + 2] = 0;
-            }
-        }
-    })?;
-    let mut white = creator
-        .create_texture_streaming(PixelFormatEnum::RGB24, tex_w as u32, tex_h as u32)
-        .map_err(|e| e.to_string())?;
-    white.with_lock(None, |buf: &mut [u8], pitch: usize| {
-        for y in 0..tex_h {
-            for x in 0..tex_w {
-                let offset = y as usize * pitch + x as usize * 3;
-                buf[offset] = 255;
-                buf[offset + 1] = 255;
-                buf[offset + 2] = 255;
-            }
-        }
-    })?;
-
+    let bg = create_colored_rect(&creator, opts.bg, tex_h, tex_w)?;
+    let fg = create_colored_rect(&creator, opts.fg, tex_h, tex_w)?;
     let mut render_rect = Rect::new(1, 1, tex_w as u32, tex_h as u32);
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.present();
+
+    // canvas.set_draw_color(Color::RGB(0, 0, 0));
+    // canvas.present();
 
     let mut comp = Chip8::new().with_mode(opts.mode);
     let prog = std::fs::read(opts.file).map_err(|e| e.to_string())?;
@@ -133,9 +133,9 @@ fn main() -> Result<(), String> {
                 render_rect.set_y(y as i32);
 
                 if comp.gfx[i] == 1 {
-                    canvas.copy(&white, None, Some(render_rect))?;
+                    canvas.copy(&fg, None, Some(render_rect))?;
                 } else {
-                    canvas.copy(&black, None, Some(render_rect))?
+                    canvas.copy(&bg, None, Some(render_rect))?
                 }
             }
             comp.draw = false;
@@ -233,4 +233,32 @@ fn handle_key(comp: &mut Chip8, key: Keycode, keyup: bool) {
     } else {
         comp.key_down(key)
     }
+}
+
+fn create_colored_rect(
+    creator: &TextureCreator<WindowContext>,
+    c: u32,
+    tex_h: u32,
+    tex_w: u32,
+) -> Result<Texture, String> {
+    let mut tex = creator
+        .create_texture_streaming(PixelFormatEnum::RGB24, tex_w, tex_h)
+        .map_err(|e| e.to_string())?;
+
+    let b = (c & 0xff) as u8;
+    let g = (c >> 8 & 0xff) as u8;
+    let r = (c >> 16 & 0xff) as u8;
+
+    tex.with_lock(None, |buf: &mut [u8], pitch: usize| {
+        for y in 0..tex_h {
+            for x in 0..tex_w {
+                let offset = y as usize * pitch + x as usize * 3;
+                buf[offset] = r;
+                buf[offset + 1] = g;
+                buf[offset + 2] = b;
+            }
+        }
+    })?;
+
+    Ok(tex)
 }
